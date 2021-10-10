479b259b5641c2a06d78698f8fe4bd3ec5777205
Owais Ahmed <oowais@gmail.com>
2021-10-10 06:43:00 +0200

    Skeleton project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 's2tw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'speech2text-web';
+}
